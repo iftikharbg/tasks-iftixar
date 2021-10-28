96a0eebe2d94b0ad87f14a02cbcffdb70e00fbd0
iftikharbg <iftikharbg@code.edu.az>
2021-10-28 14:24:01 +0400

    Homework

---
+++ Task - Copy/js/script.js
@@ -0,0 +1,93 @@
+1.// let x = document.getElementById("hi");
+// x.style.backgroundColor = "red";
+
+2.// let x = document.getElementById("drop");
+
+// for (let i = 0; i < x.length; i++) {
+//     if(x.options[i].value=='ger'){
+//         x.remove(i)
+//     }
+    
+// }
+
+3.// console.log(screen.width);
+// console.log(screen.height);
+
+4.// let x = document.getElementById("myText");
+
+// x.style.fontSize = "50px";
+
+5.//  let x = document.getElementById("myInput");
+
+//  x.addEventListener("keyup", function() {
+//      console.log(x.value);
+    
+//  })
+
+6.// let x = document.getElementById("myText");
+// x.classList.add("text");
+// x.setAttribute("title","myTitle")
+
+7.// let x = document.getElementById("myStatement")
+// x.style.backgroundColor="red";
+
+// x.addEventListener("click",()=>{
+//     x.classList.toggle("green")
+// })
+
+8.// let x = document.getElementById("sub");
+
+// x.addEventListener("click",(e)=>{
+//     e.preventDefault();
+//     let name = document.getElementsByName("fname")[0].value;
+//     let surname = document.getElementsByName("lname")[0].value;
+//     console.log(`Name:${name} Surname:${surname}`);
+
+// })
+ 
+
+9.// let x = document.getElementById("box1");
+// let y = document.getElementById("box2")
+// let c = document.getElementById("box3")
+// let b = document.getElementById("box4")
+
+
+
+// x.addEventListener("click",()=>{
+//     let box = document.createElement("div");
+//     box.textContent="box1";
+//     box.style.width="400px";
+//     box.style.height="300px";
+//     box.style.border="2px solid green";
+//     box.style.backgroundColor="orange";
+//     document.body.appendChild(box);
+// })
+
+// y.addEventListener("click",()=>{
+//     let box = document.createElement("div");
+//     box.innerHTML=`<p>box2</p>
+//                     <h1>h1</h1>`
+//     box.style.width="400px";
+//     box.style.height="300px";
+//     box.style.border="2px solid red";
+//     document.body.appendChild(box);
+// })
+
+// c.addEventListener("click",()=>{
+//     let box = document.createElement("div");
+//     box.textContent="box3"
+//     box.style.width="100px";
+//     box.style.height="100px";
+//     box.style.border="2px solid green";
+//     document.body.appendChild(box);
+// })
+
+// b.addEventListener("click",()=>{
+//     let box = document.createElement("div");
+//     box.style.width="400px";
+//     box.style.height="300px";
+//     box.style.border="2px solid red";
+//     document.body.appendChild(box);
+// })
+
+
